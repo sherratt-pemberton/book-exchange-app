1791da44f2bf8bfc7cc70501db0b7ed3d7eddb99
sherratt-pemberton <sherratt285@gmail.com>
2022-01-24 17:03:22 +1300

    initial app set up

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -4,7 +4,7 @@ function App () {
   return (
     <>
       <header className="header">
-        <h1>My Collection</h1>
+        <h1>Book Exchange</h1>
       </header>
       <section className="main">
         {/* add your code here */}
