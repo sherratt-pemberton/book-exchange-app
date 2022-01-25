8e0ce62df18952ca04c3783107356bb37c0d09c7
sherratt-pemberton <sherratt285@gmail.com>
2022-01-25 23:30:37 +1300

    set up login page front end

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,14 +1,90 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate as navigate } from 'react-router-dom'
 
 function Login () {
+
+  const blankForm = {
+  email:'',
+  password:''
+  }
+
+  const [formData, setFormData] = useState(blankForm)
+  const [errorMessage, setErrorMessage ] = useState('')
+
+  const changeHandeler = (event) => {
+    setFormData((prevFormData) =>{
+      return {
+        ...prevFormData,
+        [ event.target.name ]: [ event.target.value ]
+      }
+    })
+  } 
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setFormData( blankForm )
+  } 
+
+  const onLoginClicked = () =>{
+    alert("Login not implimented")
+  }
+
   return (
     <>
-      <header className="header">
-        <h1>Login</h1>
-      </header>
-      <section className="login">
-        {/* add your code here */}
-      </section>
+      <div>
+        <header className="header">
+          <h1>Login</h1>
+          </header>
+
+        { errorMessage && <div className= 'login-failed'>{errorMessage} </div> }
+
+        <form onSubmit={handleSubmit} className='form-body'>
+          <label className="login-form-email">Email 
+            <input
+              type= 'text'
+              name = 'email'
+              id = 'email'
+              value= {formData.email}
+              onChange = {changeHandeler}
+              className='inputForm'
+              placeholder='youremail@email.com' 
+              />  
+            </ label >
+
+          <br/><br/>
+
+          <label className="login-form-password">Password 
+            <input
+              type= 'text'
+              name = 'password'
+              id = 'password'
+              value= {formData.password}
+              onChange = {changeHandeler}
+              className='inputForm'
+              placeholder='password' 
+              />
+            </ label >
+
+          <br/><br/>
+
+          <button disabled = { !formData.email || !formData.password } onClick={ onLoginClicked }>
+            Login
+            </button>
+
+          <br/><br/>
+
+          <button onClick={ navigate( '/forgot-password' ) }>
+            Forgot Password
+            </button>
+
+          <br/><br/>
+
+          <button onClick={ navigate( '/signup' ) }>
+            Sign up
+            </button>
+
+        </form>
+      </div>
     </>
   )
 }
