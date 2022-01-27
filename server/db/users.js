46455adb6672a27ab8f310962a5e4fbd7f62348a
sherratt-pemberton <sherratt285@gmail.com>
2022-01-27 15:45:14 +1300

    added function that checks for a user in db

--- server/db/users.js
+++ server/db/users.js
@@ -4,12 +4,20 @@ function getAllUsers (db = conn) {
    return db ('users')
 }
 
+function userExsists (userEmail, db = conn) {
+   return db ('users')
+   .where('email', userEmail)
+}
+
 function addUser ( userData, db = conn){
    return db('users')
    .insert(userData)
 }
 
+
+
 module.exports = {
    getAllUsers,
+   userExsists,
    addUser
 } 
