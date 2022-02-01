f7a2667cd07f9b481527b94d53a714bd002e49c4
sherratt-pemberton <sherratt285@gmail.com>
2022-02-01 13:58:19 +1300

    posts a user to the database

--- server/db/migrations/20220126180804_requests.js
+++ server/db/migrations/20220126180804_requests.js
@@ -1,8 +1,14 @@
-
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+exports.up = (knex, Promise) => {
+   return knex.schema.createTable('requests', table => {
+     table.increments('id').primary()
+     table.integer('bookId').references('books.id')
+     table.integer('requester').references('users.id')
+     table.integer('recipient').references('users.id')
+     table.string('dateRequested')
+     table.boolean('accepted')     
+   })
+ }
+ 
+ exports.down = (knex, Promise) => {
+   return knex.schema.dropTable('requests')
+ }
--- server/routes/register.js
+++ server/routes/register.js
@@ -1,48 +1,78 @@
-import express from "express"
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
+const express = require( 'express' )
+const router = require('express').Router()
+const bcrypt = require( 'bcrypt' )
+const jwt = require( 'jsonwebtoken' )
+const db = require('../db/users')
+const { database } = require('pg/lib/defaults')
 
-const router = express.Router
+module.exports = router
 
-const {getAllUsers, addUser} = require( '..db/users')
 
-export const registerUser = () =>{
-   router.post( '/api/register', (req,res) =>{
-      const {email, password} = req.body
+//route checks for exsisting user in the database
+//if the user email does not exsist a new account is posted
+//the server then responsds with a json web token
 
-      const user = await getAllUsers().findOne({email})
+router.post ('/', async (req,res) =>{
+   const {email, password} = req.body
 
-      if (user) {
-         res.sendStatus(409)
-      }
+   console.log( req.body )
 
+   console.log( email, password)
+
+   const users = await db.getAllUsers()
+
+   console.log( "users from the database", users )
+
+   if (users.find( user => user.email == email )){
+      console.log( "email found")
+      res.sendStatus(409)
+   }
+   else{ 
       const passwordHash = await bcrypt.hash(password, 10) 
-
-      const accountInfo ={
-         firstName: req.body.firstName,
-         lastName: req.body.lastName,
-         email: req.body.email,
-         password: passwordHash,
-         street: req.body.street,
-         suburb: req.body.suburb,
-         city: req.body.city,
-         country: req.body.country,
-         postCode: req.body.postCode
+   
+      try{
+         return( 
+            db.addUser({
+               firstName: req.body.firstName,
+               lastName: req.body.lastName,
+               email: email,
+               passwordHash,         
+               isVerified: false
+            }) 
+         )
+         .then( user =>{
+            console.log( user )
+            res.sendStatus(201)
+         })
       }
+      catch ( err ) {
+         res.status(500).send(err.message)
+      }
+   }
 
-      addUser( accountInfo )
-      
-      .then(idArr =>{
-         res.json({ newId:idArr[0] })
-      })
-      .catch( err =>{
-         console.log( 'I cant do that Hall', err.message )
-         res.status (500).json({error: err.message})
-      })
 
-   } )
-
-}
-module.export ={
-   Router
-}
+   // const accountSignupInfo = {
+   //    firstName: req.body.firstName,
+   //    lastName: req.body.lastName,
+   //    email: email,
+   // }
+   
+   // .then(user => {
+      //    jwt.sign({
+      //       user:id,
+      //       accountSignupInfo,
+      //       isVerified:false
+      //    }, )
+      //    process.env.JWT_SECRET,
+      //    {
+      //       expiresIn: '2d'
+      //    },
+      //    (err, token)=>{
+      //       if( err ){
+      //          return res.status(500).send(err)
+      //       }
+      //       res.status(200).json({ token })
+      //    }
+      // })
+//   }
+})
